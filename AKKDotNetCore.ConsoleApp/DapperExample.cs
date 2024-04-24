25cf6371aa2429527f1fe76441c6c26c135a7c7a
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2024-04-24 16:57:01 +0630

    DapperExample

---
+++ AKKDotNetCore.ConsoleApp/DapperExample.cs
@@ -0,0 +1,114 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKKDotNetCore.ConsoleApp
+{
+    internal class DapperExample
+    {
+        public void Run()
+        {
+            //Read();
+            //Edit(1);
+            //Edit(15);
+            //Create("title2", "author2", "content2");
+            //Update(15, "title3", "author3", "content3");
+            Delete(15);
+        }
+
+        public void Read()
+        {
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            List<BlogDto> lst = db.Query<BlogDto>("select * from tbl_blog").ToList();
+            foreach (BlogDto item in lst)
+            {
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+                Console.WriteLine("---------------------------");
+            }
+        }
+
+        public void Edit(int id)
+        {
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            var item = db.Query<BlogDto>("select * from tbl_blog where blogid = @BlogId", new BlogDto { BlogId = id }).FirstOrDefault();
+            if (item is null)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+            Console.WriteLine(item.BlogId);
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+            Console.WriteLine("---------------------------");
+        }
+
+        public void Create(string title, string author, string content)
+        {
+            var item = new BlogDto
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+            String query = @"INSERT INTO [dbo].[Tbl_Blog]
+                ([BlogTitle]
+                ,[BlogAuthor]
+                ,[BlogContent])
+                VALUES
+                (@BlogTitle
+                ,@BlogAuthor
+                ,@BlogContent)";
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, item);
+
+            String message = result > 0 ? "Saving Successful." : "Saving Failed.";
+            Console.WriteLine(message);
+        }
+
+        public void Update(int id, string title, string author, string content)
+        {
+            var item = new BlogDto
+            {
+                BlogId = id,
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+            String query = @"UPDATE [dbo].[Tbl_Blog]
+                SET [BlogTitle] = @BlogTitle
+                ,[BlogAuthor] = @BlogAuthor
+                ,[BlogContent] = @BlogContent
+                WHERE BlogId = @BlogId";
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, item);
+
+            String message = result > 0 ? "Updating Successful." : "Updating Failed.";
+            Console.WriteLine(message);
+        }
+
+        public void Delete(int id)
+        {
+            var item = new BlogDto
+            {
+                BlogId = id
+            };
+            String query = @"DELETE FROM [dbo].[Tbl_Blog]
+                WHERE BlogId = @BlogId";
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, item);
+
+            String message = result > 0 ? "Delete Successful." : "Delete Failed.";
+            Console.WriteLine(message);
+        }
+    }
+}
--- AKKDotNetCore.ConsoleApp/Program.cs
+++ AKKDotNetCore.ConsoleApp/Program.cs
@@ -4,11 +4,15 @@ using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 
-AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
 //adoDotNetExample.Read();
-//adoDotNetExample.Create("title2", "author2", "content2");
-//adoDotNetExample.Update(13, "title3", "author3", "content3");
-//adoDotNetExample.Delete(13);
-adoDotNetExample.Edit(13);
-adoDotNetExample.Edit(12);
+// adoDotNetExample.Create("title2", "author2", "content2");
+// adoDotNetExample.Update(13, "title3", "author3", "content3");
+// adoDotNetExample.Delete(13);
+// adoDotNetExample.Edit(13);
+// adoDotNetExample.Edit(12);
+
+DapperExample dapperExample = new DapperExample();
+dapperExample.Run();
+
 Console.ReadKey();
