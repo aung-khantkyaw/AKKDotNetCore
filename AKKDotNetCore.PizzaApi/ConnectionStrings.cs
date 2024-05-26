9d2af9413cf7b9ab9f4022116d5d2a0d70d1fa06
aung-khantkyaw <aungkhantkyaw.19-20@ucsmub.edu.mm>
2024-05-26 21:09:26 +0630

    [Completed] PizzaApi

---
+++ AKKDotNetCore.PizzaApi/ConnectionStrings.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace AKKDotNetCore.PizzaApi
+{
+    internal static class ConnectionStrings
+    {
+        public static SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "Aung-Khant-Kyaw\\AUNGKHANT",
+            InitialCatalog = "DotNetTrainingBatch4",
+            UserID = "sa",
+            Password = "sasa@123",
+            TrustServerCertificate = true,
+        };
+    }
+}
---
+++ AKKDotNetCore.PizzaApi/Queries/PizzaQuery.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKKDotNetCore.WinFormsApp.Queries
+{
+    internal class PizzaQuery
+    {
+        public static string PizzaOrderQuery { get; } =
+            @"select po.*, p.Pizza, p.Price from [dbo].[Tbl_PizzaOrders] po
+            inner join Tbl_Pizza p on p.PizzaId = po.PizzaId
+            where PizzaOrderInvoiceNo = @PizzaOrderInvoiceNo;
+            ";
+
+        public static string PizzaOrderDetailQuery { get; } =
+            @"select pod.*, pe.PizzaExtraName, pe.Price from [dbo].[Tbl_PizzaOrderDetails] pod
+            inner join Tbl_PizzaExtra pe on pod.PizzaExtraId = pe.PizzaExtraId
+            where PizzaOrderInvoiceNo = @PizzaOrderInvoiceNo;";
+    }
+}
